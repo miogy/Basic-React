33b56e5a82c413903d85c3a03272e7ec415d6afc
miogy <jjangrl87@gmail.com>
2022-12-10 00:01:12 +0900

    ADD : router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./page/Home";
+import About from "./page/About";
+import Mypage from "./page/Mypage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/mypage",
+        element: <Mypage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ src/page/Mypage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Mypage() {
+  return <>Mypage</>;
+}
+
+export default Mypage;
